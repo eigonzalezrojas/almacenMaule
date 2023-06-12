ea8409e6e60cc8453077f1000e32b02cd54e8d65
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2023-06-12 16:14:36 -0400

    build shopping cart

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 //Obtenemos los id de los elementos HTML
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
-//fragmente memoria volatil 
+//fragmento memoria volatil 
 const fragment = document.createDocumentFragment()
+
+//creación del carrito, coleccion de objetos
+let carrito =  {}
 
 //Esperamos que se cargue primero todo el HTML
 document.addEventListener("DOMContentLoaded", () => {
     fetchData()
+})
+
+items.addEventListener('click', e =>{
+    addCarrito(e)
 })
 
 //Realizamos la obtención de data desde el archivo json de productos
@@ -27,9 +34,33 @@ const pintarCards = (data)=>{
         templateCard.querySelector('#precio').textContent = `$ ${producto.precio}`
         templateCard.querySelector('#cantidad').textContent = producto.medida
         templateCard.querySelector('img').setAttribute("src", producto.img)
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id
         
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     })
     items.appendChild(fragment)
+}
+
+const addCarrito = (e) =>{
+    //console.log(e.target);
+    //console.log(e.target.classList.contains('btn-dark'));
+    if (e.target.classList.contains('btn-dark')) {
+        //console.log(e.target.parentElement)
+        setCarrito(e.target.parentElement)
+    }
+    // Detenemos cualquier otro evento en items
+    e.stopPropagation()
+}
+
+const setCarrito = objeto =>{
+    //console.log(obj);
+    const producto = {
+        id : objeto.querySelector('.btn-dark').dataset.id,
+        nombre : objeto.querySelector('h5').textContent,        
+        precio : objeto.querySelector('#precio').textContent,
+        medida : objeto.querySelector('#cantidad').textContent
+    }
+    console.log(producto);
+
 }
