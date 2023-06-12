ee65a49be4fbe0eca2b514196f8004dab58327d2
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2023-06-12 15:37:16 -0400

    api json productos

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Obtenemos los id de los elementos HTML
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
+//fragmente memoria volatil 
+const fragment = document.createDocumentFragment()
 
 //Esperamos que se cargue primero todo el HTML
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,5 +22,14 @@ const fetchData = async()=>{
 }
 
 const pintarCards = (data)=>{
-    console.log(data);
+    data.forEach(producto=>{
+        templateCard.querySelector('h5').textContent = producto.nombre
+        templateCard.querySelector('#precio').textContent = `$ ${producto.precio}`
+        templateCard.querySelector('#cantidad').textContent = producto.medida
+        templateCard.querySelector('img').setAttribute("src", producto.img)
+        
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
 }
